414252359b6914c7582f0f03d7ceef34333f655c
kahina <mokrane@lyxeo.fr>
2018-11-13 21:59:58 +0100

    modification

--- Module.php
+++ Module.php
@@ -7,6 +7,7 @@ use Zend\ServiceManager\ServiceLocatorInterface;
 
 class Module extends AbstractModule
 {
+    
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
